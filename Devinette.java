212cdff9eabb3e3b1b7d255d70d3b43091be500d
Anis19088 <122814426+Anis19088@users.noreply.github.com>
2023-10-08 22:02:10 +0200

    Add files via upload

---
+++ Devinette.java
@@ -0,0 +1,35 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Devinette {
+    boolean trouver = false;
+    int test; // Variable d'instance
+
+    public void devinette() {
+        Scanner scanner = new Scanner(System.in);
+
+        Random random = new Random();
+        int rand = random.nextInt(100);
+        System.out.println(rand);
+        while (!trouver) {
+
+
+            System.out.println("Saisissez un nombre :");
+            test = scanner.nextInt(); // Utilisez la variable d'instance
+
+            if (test > rand) {
+                System.out.println("C'est moins");
+            } else if (test < rand) {
+                System.out.println("C'est plus");
+            } else {
+                trouver = true;
+                System.out.println("C'est trouvé ! Bravo ! Voici le nombre saisi : " + test + " et voici le nombre du rand " + rand);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Devinette dev = new Devinette();
+        dev.devinette();
+    }
+}
